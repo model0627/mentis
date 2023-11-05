5d9f732256030194cd67f2c5b67613b5b3d10344
shawn <model0627@gmail.com>
2023-11-05 23:51:28 +0900

    navigator

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -2,8 +2,13 @@
 
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from 'lucide-react';
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/spinner";
+import { SignInButton } from "@clerk/clerk-react";
+import Link from "next/link";
 
 export const Heading = () => {
+    const { isAuthenticated, isLoading} = useConvexAuth();
     return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -12,10 +17,27 @@ export const Heading = () => {
             <h3 className="text-base sm:text-xl md:text-2xl font-medium">
                 Sotion is the connected workspace where <br /> better, faster work happens.
             </h3>
-            <Button>
-                Enter Sotion
-                <ArrowRight className="h-4 w-4 ml-2" />
-            </Button>
+            {isLoading && (
+                <div className="w-full flex items-center justify-center">
+                    <Spinner size="lg" />
+                </div>
+            )}
+            {isAuthenticated && !isLoading && (
+                <Button asChild>
+                    <Link href="/documents">
+                        Enter Sotion
+                    <ArrowRight className="h-4 w-4 ml-2" />
+                    </Link>
+                </Button>
+            )}
+            {!isAuthenticated && !isLoading && (
+                <SignInButton mode="modal">
+                    <Button>
+                        Get Sotion free
+                        <ArrowRight className="h-4 w-4 ml-2" />
+                    </Button>
+                </SignInButton>
+            )}
         </div>
     )
 }
--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -5,8 +5,10 @@ import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
 import { useConvexAuth } from "convex/react";
-import { SignInButton } from "@clerk/clerk-react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 export const Navbar =() => {
     const { isAuthenticated, isLoading} = useConvexAuth();
@@ -17,7 +19,7 @@ export const Navbar =() => {
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
                 {isLoading && (
-                    <p>Loading...</p> 
+                    <Spinner />
                 )}
                 {!isAuthenticated && !isLoading && (
                     <>
@@ -32,6 +34,16 @@ export const Navbar =() => {
                             </Button>
                         </SignInButton>
 
+                    </>
+                )}
+                {isAuthenticated && !isLoading && (
+                    <>
+                        <Button variant="ghost" size="sm" asChild>
+                            <Link href="/documents">
+                                Enter Sotion
+                            </Link>
+                        </Button>
+                        <UserButton afterSignOutUrl="/" />
                     </>
                 )}
                 <ModeToggle />
