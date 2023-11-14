0c96822f1a6be33714e2d768c1bd1f4eeccab380
shawn <model0627@gmail.com>
2023-11-14 16:34:30 +0900

    rollback

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,8 +1,28 @@
 import { NextResponse } from 'next/server';
-
-export async function handler() {
-  
+ 
+export async function GET() {
+  try {
+    const result = await fetch(
+      'https://api.exchangerate.host/convert?from=USD&to=EUR',
+    );
+    const data = await result.json();
+ 
+    if (data.success) {
+      const rate = data.info.rate;
+      console.log(`Latest exchange rate (USD to EUR): ${rate}`);
+      return NextResponse.json(
+        { message: `Exchange rate (USD to EUR) is ${rate}` },
+        { status: 200 },
+      );
+    } else {
+      console.error('Error fetching exchange rate:', data);
+      return NextResponse.json(
+        { error: 'Failed to fetch exchange rate' },
+        { status: 500 },
+      );
+    }
+  } catch (error) {
+    console.error('Error fetching exchange rate:', error);
     
-    console.error('Error fetching exchange rate:');
-    
+  }
 }
