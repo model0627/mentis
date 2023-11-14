5cdfc69acdaf0b952677dc24853fe27c46a6afc4
shawn <model0627@gmail.com>
2023-11-14 16:33:51 +0900

    add

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,31 +1,8 @@
 import { NextResponse } from 'next/server';
- 
-export async function GET() {
-  try {
-    const result = await fetch(
-      'https://api.exchangerate.host/convert?from=USD&to=EUR',
-    );
-    const data = await result.json();
- 
-    if (data.success) {
-      const rate = data.info.rate;
-      console.log(`Latest exchange rate (USD to EUR): ${rate}`);
-      return NextResponse.json(
-        { message: `Exchange rate (USD to EUR) is ${rate}` },
-        { status: 200 },
-      );
-    } else {
-      console.error('Error fetching exchange rate:', data);
-      return NextResponse.json(
-        { error: 'Failed to fetch exchange rate' },
-        { status: 500 },
-      );
-    }
-  } catch (error) {
-    console.error('Error fetching exchange rate:', error);
-    return NextResponse.json(
-      { error: 'Failed to fetch exchange rate' },
-      { status: 500 },
-    );
-  }
+
+export async function handler() {
+  
+    
+    console.error('Error fetching exchange rate:');
+    
 }
